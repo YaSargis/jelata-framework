606a2088edfeb4a72a779c79ef6942698305d29f
Sargis Kazaryan <yujniybro@yandex.ru>
2021-03-15 20:45:57 +0300

    languages ant

---
+++ files/languages.js
@@ -0,0 +1,140 @@
+var LaNg = {
+	
+	"unknownError" : {
+		"EN":"Unknown Error",
+		"RU":"Произошла неизвестная ошибка"
+	},
+	"Error": {
+		"EN":"Error",
+		"RU":"Ошибка"
+	},
+	"passwordPlaceholder" : {
+		"EN":"password",
+		"RU":"пароль"
+	},
+	"timePlaceholder" : {
+		"EN":"Chose time",
+		"RU":"Выберите время"
+	},
+	"certList":{
+		"EN":"list:",
+		"RU":"Список сертификатов:"
+	},
+	"certButton": {
+		"EN":"Сhoose certificate",
+		"RU":"Выберите сертификат"
+	},
+	"multiSelect" : {
+		"EN":"Choose from the list",
+		"RU":"Выберите из списка"
+	},
+	"typeAHead": {
+		"EN":"start typing",
+		"RU":"Введите поисковой запрос"
+	},
+	"NOresult": {
+		"EN":"NO RESULT",
+		"RU":"Нет данных"
+	},
+	"NOresult": {
+		"EN":"NO RESULT",
+		"RU":"Нет данных"
+	},
+	"signError": {
+		"EN":"sign error",
+		"RU":"ошибка подписи"
+	},
+	"Message": {
+		"EN":"Message",
+		"RU":"Сообщение"
+	},
+	"wsError": {
+		"EN":"ws error",
+		"RU":"ошибка подключения к веб-сокетам"
+	},
+	"reportGenerate": {
+		"EN":"Generate",
+		"RU":"Сформировать"
+	},
+	"reportNotGet": {
+		"EN":"Method are not GET",
+		"RU":"Метод не GET"
+	},
+	"goBack": {
+		"EN":"go back",
+		"RU":"вернуться назад"
+	},
+	"noCryptoPlugin": {
+		"EN":"Can not found the module",
+		"RU":"не удалось обнаружить КриптоПРО плагин"
+	},
+	
+	"plLogin": {
+		"EN":"login",
+		"RU":"логин"
+	},
+	
+	"loginForm": {
+		"EN":"Log in",
+		"RU":"Авторизация"
+	},
+	
+	"signIn": {
+		"EN":"sign in",
+		"RU":"вход"
+	},
+	
+	"oOr": {
+		"EN":"or",
+		"RU":"или"
+	},
+	
+	"logSig": {
+		"EN":"Log in by digital signature",
+		"RU":"авторизацоваться по ЭЦП"
+	},
+	
+	"logPas": {
+		"EN":"Log in by login/password",
+		"RU":"авторизацоваться по логину/паролю"
+	},
+	"tCount" : {
+		"EN":"count",
+		"RU":"строк"
+	},
+	"filterOK":{
+		"EN":"ok",
+		"RU":"применить"
+	}
+	,
+	"filterClean":{
+		"EN":"clean",
+		"RU":"очистить"
+	},
+	"bClose": {
+		"EN":"close",
+		"RU":"закрыть"
+	},
+	"shCols": {
+		"EN":"show/hide columns",
+		"RU":"показать/скрыть столбцы"
+	},
+	"notFound": {
+		"EN":"404: Not Found",
+		"RU":"404: Страница не найдена"
+	},
+	"Yes": {
+		"EN":"Yes",
+		"RU":"Да"
+	},
+	"Confirm": {
+		"EN":"confirm",
+		"RU":"подтверждение"
+	}, 
+	"notForget" : {
+		"EN":"Do not forget save changes",
+		"RU":"Не забудьте сохранить изменения"
+	}
+}
+
+var LnG = 'EN'
